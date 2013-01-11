4e80250f57e0e2a0b0974cf3864d27364348db2f
Daniel <daxx4182@gmail.com>
2013-01-11 09:45:28 +0800

    Update some PUB changes

--- pick.sh
+++ pick.sh
@@ -1,8 +1,5 @@
 # Run me for AOKP's Unicorn Butta
 
-cd ../frameworks/base
-git fetch http://gerrit.sudoservers.com/AOKP/frameworks_base refs/changes/33/4633/2 && git cherry-pick FETCH_HEAD
-
 cd ../av
 git fetch http://gerrit.sudoservers.com/AOKP/frameworks_av refs/changes/34/4634/1 && git cherry-pick FETCH_HEAD
 git fetch http://gerrit.sudoservers.com/AOKP/frameworks_av refs/changes/15/4615/2 && git cherry-pick FETCH_HEAD
