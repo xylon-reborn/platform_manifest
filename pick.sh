3bbbb3799882f6b71a919d18f6320ae89428d19e
Daniel <daxx4182@gmail.com>
2013-01-13 23:05:26 +0800

    Remove it until we need it.

--- pick.sh
+++
@@ -1,8 +0,0 @@
-# Run me for AOKP's Unicorn Butta
-
-cd ../frameworks/av
-git fetch http://gerrit.sudoservers.com/AOKP/frameworks_av refs/changes/34/4634/1 && git cherry-pick FETCH_HEAD
-git fetch http://gerrit.sudoservers.com/AOKP/frameworks_av refs/changes/15/4615/2 && git cherry-pick FETCH_HEAD
-
-cd ../native
-git fetch http://gerrit.sudoservers.com/AOKP/frameworks_native refs/changes/35/4635/1 && git cherry-pick FETCH_HEAD
